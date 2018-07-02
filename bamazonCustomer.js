2754d3785e27ed67709ae72658160f58467be013
unknown <victorha@gmail.com>
2018-07-02 15:31:32 -0400

    initial questions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,26 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+
+inquirer.prompt([
+    {
+        type: "list",
+        name: "product",
+        message: "What would you like to buy?",
+        choices: ["test 1", "test 2"]
+    }, 
+    {
+        message: "How many would you like to purchase?",
+        type: "input",
+        name: "quantity",
+        default: "number"
+    }
+
+    
+])
+
+.then(
+    function(answers) {
+        console.log(answers.product);
+        console.log(answers.quantity)
+    }
+);
