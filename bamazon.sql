6f85bbf98e84a2a1b9aa6a012fdbfa01d8db3a35
unknown <victorha@gmail.com>
2018-07-14 21:26:56 -0400

    some questions added

--- bamazon.sql
+++ bamazon.sql
@@ -3,10 +3,20 @@ use bamazon;
 
 create table products (
     item_id integer primary key auto_increment,
-    product_name varchar(50), not null
+    product_name varchar(50) not null,
     department_name varchar(10),
     price float not null, --Sales cost 
     stock_quantity integer(2)
 );
 
-insert into products (product_name, department_name, price, stock_quantity) values (); --inputsome dummy data later
+insert into products (product_name, department_name, price, stock_quantity) values 
+    ("external battery", "electronics", 48, 10), 
+    ("external harddrive", "electronics", 90, 10),
+    ("monitor", "electronics", 180, 8),
+    ("settlers of catan", "board games", 53, 5),
+    ("consentacle", "board games", 30, 3),
+    ("dead of winter", "board games", 90, 4),
+    ("agricola", "board games", 67, 6),
+    ("red rising", "books", 17, 2),
+    ("thinking fast and slow", "books", 15, 3),
+    ("name of the wind", "books", 11, 4); 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,26 +1,54 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root"
+});
 
-inquirer.prompt([
-    {
-        type: "list",
-        name: "product",
-        message: "What would you like to buy?",
-        choices: ["test 1", "test 2"]
-    }, 
-    {
-        message: "How many would you like to purchase?",
-        type: "input",
-        name: "quantity",
-        default: "number"
-    }
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected");
+});
 
-    
-])
+buyProduct();
 
-.then(
-    function(answers) {
-        console.log(answers.product);
-        console.log(answers.quantity)
-    }
-);
+function buyProduct() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "product",
+            message: "What would you like to buy? \n 1 external battery \n 2 external harddrive \n 3 monitor \n 4 settlers of catan \n 5 consentacle \n 6 dead of winter \n 7 agricola \n 8 red rising \n 9 thinking fast and slow \n 10 name of the wind",
+            choices: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        }, 
+        {
+            message: "How many would you like to purchase?",
+            type: "input",
+            name: "quantity",
+            default: "number"
+        }
+    ])
+    .then(
+        function(answers) {
+            var quantity = parseInt(answers.quantity);
+            var product = answers.product;
+            con.query("Select * from INVENTORY", function(err, result, fields) {
+                if (err) throw err;
+                var results = result;
+                console.log(results);
+            });
+
+            /*if (typeof quantity !== "number") {
+                console.log("That is not a number");
+            }
+            else {
+                con.query("UPDATE INVENTORY SET product_quantity = " //grab quantity from sql
+                +product+" WHERE item_id = ") //grab item id
+                console.log(answers.product);
+                console.log(answers.quantity)
+            }*/
+        }
+    );
+    end();
+};
+//connection.end();
