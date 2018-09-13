7dec56d7d1019d3787ea3a15bb11e222b772f38d
unknown <victorha@gmail.com>
2018-09-13 12:49:05 -0400

    fixed NaN detection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,7 +19,7 @@ con.connect(function(err) {
         {
             type: "list",
             name: "product",
-            message: `What would you like to buy? \n 1 external battery $48 \n 2 external harddrive $90 \n 3 monitor $180 \n 4 settlers of catan $53 \n 5 consentacle $30 \n 6 dead of winter $90 \n 7 agricola $67 \n 8 red rising $17 \n 9 thinking fast and slow $15 \n 10 name of the wind $11`,
+            message: `What would you like to buy? \n 0 external battery $48 \n 1 external harddrive $90 \n 2 monitor $180 \n 3 settlers of catan $53 \n 4 consentacle $30 \n 5 dead of winter $90 \n 6 agricola $67 \n 7 red rising $17 \n 8 thinking fast and slow $15 \n 9 name of the wind $11`,
             choices: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
         },  
         {
@@ -32,30 +32,25 @@ con.connect(function(err) {
     .then(
         function(answers) {
             var quantity = parseInt(answers.quantity);
+            console.log(typeof(quantity));
 
-            if (typeof quantity !== "number") {
+            if (isNaN(quantity)) {
                 console.log("That is not a number");
             }
 
-            var product = answers.product;
+            var product = (answers.product-1);
+
             con.query("Select * from PRODUCTS", function(err, result, fields) {
                 if (err) throw err;
                 if (quantity > result[product].stock_quantity) {
                     console.log("Insufficient quantity!")
                 }
+                else {
+                    var totalCost = (result[product].price * quantity);
+                    console.log(`You have purchased ${quantity} of ${result[product].product_name} for a total of ${totalCost}.`)
+                    con.query("UPDATE PRODUCTS SET stock_quantity = "+ (result[product].stock_quantity - quantity )+" WHERE item_id = "+product)
+                }
             });
-
-
-
-
-            //     con.query("UPDATE PRODUCTS SET stock_quantity = " 
-            //     +quantity+" WHERE item_id = "+product) //need to add logic here to subtract if
-            //     console.log(answers.product);
-            //     console.log(answers.quantity)
-
         }
     );
-    //end();
-//};
-//connection.end();
 });
